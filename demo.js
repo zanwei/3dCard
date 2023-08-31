c1c939eeeee826e3c7957bbb98641ef978f773a1
zanwei.guo <40333372+zanwei@users.noreply.github.com>
2023-08-31 12:20:45 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,109 @@
+//场景设置
+const scene = new THREE.Scene();
+
+//渲染器设置  
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor(0xffffff); // 设置背景色为白色
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//相机设置
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt(scene.position);
+
+//光照设置
+const light = new THREE.DirectionalLight(0xffffff, 1); 
+light.position.set( 0, 0, 10);
+scene.add(light);
+
+//加载纹理
+const textureLoader = new THREE.TextureLoader();
+const mapFront = textureLoader.load('card-front.jpg', function(texture) {
+  texture.minFilter = THREE.LinearFilter;
+  texture.magFilter = THREE.LinearFilter;
+}); 
+const mapBack = textureLoader.load('card-back.jpg', function(texture) {
+  texture.minFilter = THREE.LinearFilter;
+  texture.magFilter = THREE.LinearFilter;
+}); 
+
+//材质设置
+const materialFront = new THREE.MeshPhongMaterial({
+  map: mapFront,
+  shininess: 600,
+  metalness: 2
+});
+
+const materialBack = new THREE.MeshPhongMaterial({
+  map: mapBack,
+  shininess: 600,
+  metalness: 2
+});
+
+//卡片网格模型
+const geometry = new THREE.BoxGeometry(40, 26, 0.1);
+
+const meshFront = new THREE.Mesh(geometry, materialFront);
+meshFront.position.z = 0.05; //微调位置，使正反面不重叠
+
+const meshBack = new THREE.Mesh(geometry, materialBack);
+meshBack.position.z = -0.05; //微调位置，使正反面不重叠
+
+//使用 THREE.Group 来包含正反面
+const card = new THREE.Group();
+card.add(meshFront);
+card.add(meshBack);
+scene.add(card);
+
+//添加旋转控制
+let rotate = false;
+let startMouseX = 0;
+let startMouseY = 0;
+let startCardRotation = 0;
+let startCardRotationX = 0;
+
+document.addEventListener('mousedown', e => {
+  rotate = true;
+  startMouseX = e.pageX;
+  startMouseY = e.pageY;
+  startCardRotation = card.rotation.y;
+  startCardRotationX = card.rotation.x; 
+});
+
+document.addEventListener('mouseup', e => {
+  rotate = false;
+});
+
+document.addEventListener('mousemove', e => {
+  if(rotate){
+    const deltaX = (e.pageX - startMouseX) / window.innerWidth;
+    const deltaY = (e.pageY - startMouseY) / window.innerHeight;
+    card.rotation.y = startCardRotation + deltaX * 2 * Math.PI; 
+    card.rotation.x = startCardRotationX + deltaY * 2 * Math.PI;
+  }
+});
+
+//添加物理引擎
+let velocity = 0;  // 速度
+let acceleration = 0.05;  // 加速度
+
+function animate() {
+  requestAnimationFrame( animate );
+  
+  // 在拖拽开始和结束时，使用物理引擎实现缓入缓出效果
+  if (rotate) {
+    velocity += acceleration;
+  } else {
+    velocity -= acceleration;
+  }
+  velocity = Math.max(0, Math.min(1, velocity));  // 限制速度在 0 和 1 之间
+  
+  card.rotation.y += 0.01 * velocity;
+
+  // 添加缓慢的自动旋转
+  card.rotation.y += 0.001;  
+
+  renderer.render( scene, camera );
+}
+animate();
